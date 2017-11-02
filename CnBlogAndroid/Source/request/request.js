6fdd7987d101ea2a1cdf71e932f525364aeabb36
Issac-Newton <1556820213@qq.com>
2017-11-02 09:08:02 +0800

    modify the request to adjust all the sturcture

--- CnBlogAndroid/Source/request/request.js
+++ CnBlogAndroid/Source/request/request.js
@@ -1,12 +1,18 @@
 import Config from '../config';
 import api from '../api/api.js';
 import {authData} from '../config'
+import {StorageKey} from '../config'
+import * as storage from '../Storage/storage.js'
+
 import {
-    ToastAndroid
+    ToastAndroid,
+	AsyncStorage,
 }from 'react-native';
 
+/**
 function GetToken(){
     //先获取token，然后再获取信息
+	var token = storage.getItem("USER_TOKEN");
     var Body = "client_id=" + authData.clientId + "&client_secret=" + authData.clientSecret + "&grant_type=client_credentials";
     return new Promise((resolve,reject)=>{
         fetch(Config.AccessToken,{
@@ -18,14 +24,14 @@ function GetToken(){
         })
         .then((response)=>response.json())
         .then((responseJson)=>{
-            resolve("Bearer" + " " + responseJson.access_token);
+            resolve("Bearer" + " " + token);
         })
         .catch((error) => {
             throw error;
             reject();
         });
     });
-}
+}*/
 
 //这里修改为返回Promise对象(by ZiJiaW)
 function GetInfo(url, token){
@@ -34,12 +40,11 @@ function GetInfo(url, token){
             method : 'GET',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': token,
+                'Authorization': 'Bearer' + ' ' + token,
             },
         })
         .then((response)=>response.json())
         .then((jsonData)=>{
- //           ToastAndroid.show("222",ToastAndroid.SHORT);
             resolve(jsonData);
         })
         .catch((error) => {
@@ -51,18 +56,16 @@ function GetInfo(url, token){
 //异步依赖异步回调的Primise用法 参考https://segmentfault.com/a/1190000005894077?_ea=943171
 //这里将上面两个异步作了进一步封装(by ZiJiaW)，promise返回值为该url的json对象
 export function Get(url){
-    return new Promise((resolve,reject)=>{
-        GetToken()
-        .then((token)=>{
-            return GetInfo(url, token);
-        })
-        .then((jsonData)=>{
- //           ToastAndroid.show("111",ToastAndroid.SHORT);
-            resolve(jsonData)
-        })
-        .catch((error) => {
-            console.error(error);
-            reject();
-        });
-    })
+	return new Promise((resolve,reject)=>{	
+		storage.getItem(StorageKey.USER_TOKEN).then((token)=>{
+			return GetInfo(url, token.access_token);
+		})
+		.then((jsonData)=>{
+			resolve(jsonData)
+		})
+		.catch((error) => {
+			console.error(error);
+			reject();
+		});
+	})
 }
